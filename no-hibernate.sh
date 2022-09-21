be532224190e264f156c6f094a3e0d82de688de0
some0necoding <codingandprogramming.code@gmail.com>
2022-09-21 18:32:27 +0200

    adding new script

---
+++ no-hibernate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LOGIND_CONFIG_FILE="./bin/no-hibernate-1.conf"
+SLEEP_CONFIG_FILE="./bin/no-hibernate-2.conf"
+
+LOGIND_DIR="/etc/systemd/logind.conf.d"
+SLEEP_DIR="/etc/systemd/sleep.conf.d"
+
+FILE_NAME="no-hibernate.conf"
+
+copy_files () {
+
+    echo "Creating directories..."
+
+    sudo mkdir -p $LOGIND_DIR $SLEEP_DIR
+
+    echo "Copying files..."
+
+    sudo cp $LOGIND_CONFIG_FILE "$LOGIND_DIR/$FILE_NAME"
+    sudo cp $SLEEP_CONFIG_FILE "$SLEEP_DIR/$FILE_NAME"
+
+    echo "Done"
+}
+
+copy_files
