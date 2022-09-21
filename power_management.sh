1a6a1cc17ab266d2c95f5d38380a6b3e97647e39
some0necoding <codingandprogramming.code@gmail.com>
2022-09-21 16:37:16 +0200

    improving power management script efficiency

--- power_management.sh
+++ power_management.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+check_powertop () {
+
+    local ret_value
+
+    echo "Looking for powertop utility..."
+
+    (pacman -Ss powertop) >> /dev/null 2>&1
+
+    ret_value=$?
+
+    if [[ $ret_value != 0 ]]; then
+        install_powertop
+        return $?
+    else
+        return 0
+    fi
+}
+
 install_powertop () {
 
     local ret_value
@@ -13,10 +31,10 @@ install_powertop () {
     if [[ $ret_value != 0 ]]; then
         echo "Error: cannot download powertop package"
         return $ret_value
+    else
+        echo "Done"
+        return 0
     fi
-
-    echo "Done"
-    return 0
 }
 
 tune_settings () {
@@ -32,23 +50,22 @@ tune_settings () {
     if [[ $ret_value != 0 ]]; then
         echo "Error: something went wrong"
         return $ret_value
+    else
+        echo "Done"
+        return 0
     fi
-
-    echo "Done"
-    return 0
 }
 
 run_configuration () {
 
     local ret_value
 
-    install_powertop
+    check_powertop
 
-    ret_value=$?; [[ $ret_value != 0 ]] && return ret_value
+    ret_value=$?; [[ $ret_value != 0 ]] && exit $ret_value
 
     tune_settings
-
-    ret_value=$?; [[ $ret_value != 0 ]] && return ret_value || return 0
+    exit $?
 }
 
 run_configuration
