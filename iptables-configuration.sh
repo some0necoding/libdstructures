1c990bf9d170dcc8d8edec9cf5af1ed3267607bb
some0necoding <codingandprogramming.code@gmail.com>
2022-09-25 10:39:04 +0200

    commenting

--- iptables-configuration.sh
+++ iptables-configuration.sh
@@ -24,43 +24,44 @@ check_root () {
 
 # Create TCP and UDP chains
 create_chains () {
-    sudo iptables -N TCP
-    sudo iptables -N UDP
+    iptables -N TCP
+    iptables -N UDP
 }
 
 # Set default policy of INPUT FORWARD and DROP chains
 set_policy () {
-    sudo iptables -P INPUT DROP
-    sudo iptables -P FORWARD DROP
-    sudo iptables -P OUTPUT ACCEPT
+    iptables -P INPUT DROP
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT ACCEPT
 }
 
 # Set custom firewall rules
 set_rules () {
-    sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-    sudo iptables -A INPUT -i lo -j ACCEPT
-    sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-    sudo iptables -A INPUT -p icmp -m icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
-    sudo iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
-    sudo iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m conntrack --ctstate NEW -j TCP
-    sudo iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
-    sudo iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
-    sudo iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
-    sudo iptables -A TCP -p tcp -m tcp --dport 80 -j DROP
-    sudo iptables -A TCP -p tcp -m tcp --dport 8080 -j DROP
-    sudo iptables -A TCP -p tcp -m tcp --dport 443 -j ACCEPT
-    sudo iptables -A TCP -p tcp -m tcp --dport 22 -j DROP
-    sudo iptables -A TCP -p tcp -m tcp --dport 53 -j ACCEPT
-    sudo iptables -A UDP -p udp -m udp --dport 53 -j ACCEPT
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+    iptables -A INPUT -p icmp -m icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
+    iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m conntrack --ctstate NEW -j TCP
+    iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+    iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+    iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+    iptables -A TCP -p tcp -m tcp --dport 80 -j DROP
+    iptables -A TCP -p tcp -m tcp --dport 8080 -j DROP
+    iptables -A TCP -p tcp -m tcp --dport 443 -j ACCEPT
+    iptables -A TCP -p tcp -m tcp --dport 22 -j DROP
+    iptables -A TCP -p tcp -m tcp --dport 53 -j ACCEPT
+    iptables -A UDP -p udp -m udp --dport 53 -j ACCEPT
 }
 
 # Apply changes system-wide and start/enable iptables
 apply_changes () {
-    sudo iptables-save -f $IPTABLES_RULES
-    sudo systemctl start iptables
-    sudo systemctl enable iptables
+    iptables-save -f $IPTABLES_RULES
+    systemctl start iptables
+    systemctl enable iptables
 }
 
+# Main function
 run_iptables_config () {
 
     # Checking for root
