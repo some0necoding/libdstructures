205f93997e53eb00c943a4220f19cb43fe7d458d
some0necoding <codingandprogramming.code@gmail.com>
2023-10-24 14:47:30 +0200

    improving the specification of dynarr

--- src/dynarr.c
+++ src/dynarr.c
@@ -6,17 +6,22 @@
  *
  *      nothing.
  *
- *      Everything is implemented as macros in his header file because C does
- *      not support generics but I still want it to do so.
- *
- *      Remember that this implementation is not really type safe (for obvious
- *      reasons i'd say), so be careful.
+ *      Everything is implemented as macros in its header file because C does
+ *      not support generics but I still want it to do so. For this reason
+ *      you should remember that this implementation is not really type safe,
+ *      so be careful.
  *
  *      dynarr is a simple implementation of a dynamic array in C (i.e. an
- *      array of non-fixed size).
+ *      array of non-fixed size). A dynarr is stretchable but not shrinkable.
+ *      This is to reduce overhead of memory allocation.
  *
  *      The dynarr itself is defined as an anonymous struct containing a
- *      header (dynarr_header) followed by an array of data.
+ *      header (dynarr_header) followed by an array of data. Because of this
+ *      be aware that a dynarr WILL EXIST ONLY INSIDE THE SCOPE IT IS DEFINED
+ *      IN. If you need to pass a dynarr between functions you will need to
+ *      transform it in a fixed-size array and then to retransform it back
+ *      in a dynarr using respectively the functions dynarr_to_arr() and
+ *      arr_to_dynarr() (See header file for their specifications).
  *
  *      A dynarr can be defined (NOT initialized) as follows:
  *
